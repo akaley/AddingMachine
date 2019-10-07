5e9dd006c39c4bc6292f6f45aa4a90679e412da4
da_as <da_as@DESKTOP-U2N87CP>
2019-10-06 22:53:08 -0700

    source, forgot to add

---
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,82 @@
+//Name: Ashton Kaley
+//Class: CSE 360
+//StudentID: 1211388565
+//Assignment: Assignment #2
+//Description: This is the AddingMachine Class file, containing functions that
+// get the total, add, subtract, send to string, and clear. This assignment is
+// meant to give experience with a version control tool.
+
+package cse360assign2;
+
+/**
+ * contains 6 methods {@link AddingMachine}, {@link getTotal}, {@link add},
+ *  {@link subtract}, {@link toString}, and {@link clear}. This class is meant
+ *  to perform addition and subtraction, as well as return totals.
+ * 
+ * @method AddingMachine	constructor for AddingMachine
+ * @method getTotal			returns total
+ * @method add				performs addition given an integer
+ * @method subtract			performs subtraction given an integer
+ * @method toString			converts item to string
+ * @method clear			clears console
+ */
+public class AddingMachine {
+	private int total;
+	
+	/**
+	 * Is the constructor method for {@link public class AddingMachine} and is meant to
+	 * create new AddingMachine objects
+	 */
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Is meant to retrieve the total from AddingMachine class and return it
+	 * 
+	 * @return	returns the total 
+	 */
+	public int getTotal ()
+	{
+		return 0;
+	}
+	
+	/**
+	 * Is meant to add an integer to the total given an integer
+	 * 
+	 * @param value	is the passed integer value to be added to the total
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/**
+	 * Is meant to subtract an integer from the total, given an integer value
+	 * 
+	 * @param value is the passed integer value to be subtracted from the total
+	 */
+	public void subtract (int value)
+	{
+		
+	}
+		
+	/**
+	 * Is meant to convert an int to a string for output
+	 * 
+	 * @return	returns converted string
+	 */
+	public String toString ()
+	{
+		return "";
+	}
+
+	/**
+	 * Is meant to clear the total and reset it
+	 */
+	public void clear()
+	{
+	
+	}
+}
